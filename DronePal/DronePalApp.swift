b16644b575f566b64ef693b823fe7392aa22509e
Scalegj <244308@esj.org>
2025-06-07 23:05:26 -0400

    Initial Commit

---
+++ DronePal/DronePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DronePalApp.swift
+//  DronePal
+//
+//  Created by Gregory Scales on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DronePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
